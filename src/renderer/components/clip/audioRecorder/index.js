82b6b22fed7955451749e2b1ad64fb59ec51fd71
DaryKiri <darykiri1@gmail.com>
2020-01-25 18:29:16 +0100

    Added audio recording visualizer

--- src/renderer/components/clip/audioRecorder/index.js
+++ src/renderer/components/clip/audioRecorder/index.js
@@ -2,6 +2,8 @@ import React from "react";
 import { Button, message, Card } from "antd";
 import "antd/dist/antd.css"; // or 'antd/dist/antd.less'
 import AudioPlayer from "react-h5-audio-player";
+import { visualize } from "./visualizer";
+import "../../../css/audioRecorder.css";
 import "react-h5-audio-player/lib/styles.css";
 
 // import { ipcRenderer } from "electron";
@@ -11,6 +13,7 @@ export function AudioRecorder({ setAudioDataURL }) {
   const [mediaRecorder, setMediaRecorder] = React.useState({});
   const [chunks, setChunks] = React.useState([]);
   const [audioURL, setAudioURL] = React.useState("");
+  const canvasEl = React.useRef(null);
 
   const updateAudioURL = base64String => {
     setAudioDataURL(base64String);
@@ -24,6 +27,7 @@ export function AudioRecorder({ setAudioDataURL }) {
   const handleStream = stream => {
     const mediaRecorder = new MediaRecorder(stream);
     setMediaRecorder(mediaRecorder);
+    visualize(stream, canvasEl.current);
     mediaRecorder.start();
 
     mediaRecorder.ondataavailable = function(e) {
@@ -64,6 +68,7 @@ export function AudioRecorder({ setAudioDataURL }) {
           }
         }
       });
+      setRecording(true);
       handleStream(stream);
     } catch (e) {
       message.error("Can't record audio");
@@ -73,6 +78,7 @@ export function AudioRecorder({ setAudioDataURL }) {
 
   return (
     <Card>
+      {recording ? <canvas ref={canvasEl}></canvas> : <></>}
       <Button onClick={takeScreenshot}>Record Audio</Button>
       <Button
         onClick={() => {
