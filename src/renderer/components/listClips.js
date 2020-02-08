bc41bd76929553e1980fdca325ae3f44e88b2716
Daryl Serrano Hipolito <darylserrano39@gmail.com>
2020-02-08 14:23:26 +0100

    Feature/manual clip add (#8)
    * Added files for electron-webpack
    
    - Changed to yarn and installed electron-webpack
    
    * Updated packages
    
    - electron and webpack
    
    * Moved main code to typescript
    
    * Changed package to use for exporting tsv
    
    * Fixed renderer compilation
    
    * Removed babel packages
    
    * Implement Add CLip manually

--- src/renderer/components/listClips.js
+++ src/renderer/components/listClips.js
@@ -1,26 +1,28 @@
 import React from "react";
-import { Timeline } from "antd";
+import { Timeline, Layout } from "antd";
 import { Clip } from "./clip";
 import "antd/dist/antd.css"; // or 'antd/dist/antd.less'
 
+let { Content } = Layout;
+
 export function ListClip({ data, onEdit, onDelete, listening }) {
   return (
-  
-    <Timeline>
-      {data.map((clipData, index) => {
-        return (
-          <Timeline.Item key={index.toString()}>
-            <Clip
-              id={index}
-              data={clipData}
-              onEdit={onEdit}
-              onDelete={onDelete}
-            ></Clip>
-          </Timeline.Item>
-        );
-      })}
-    </Timeline>
-   
+    <Content style={{ "text-align": "left" }}>
+      <Timeline>
+        {data.map((clipData, index) => {
+          return (
+            <Timeline.Item key={index.toString()}>
+              <Clip
+                id={index}
+                data={clipData}
+                onEdit={onEdit}
+                onDelete={onDelete}
+              ></Clip>
+            </Timeline.Item>
+          );
+        })}
+      </Timeline>
+    </Content>
   );
   // return (
   //   <>
